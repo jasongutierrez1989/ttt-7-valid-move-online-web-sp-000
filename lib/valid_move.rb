2126bcc57094239ac175d64a6323d6018259f917
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-11-11 03:12:16 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken?(board, index) == true && index >= 0 && index <= 8
-    return false
-  else
+  if position_taken?(board, index) == false && index >= 0 && index <= 8
     return true
+  else
+    return false
   end
 end
 
